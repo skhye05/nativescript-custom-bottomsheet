6e00ebd7cea8a539a41f13d70ee462d28fa6ad2c
Costa Caravotas <costacaravotas@Costas-iMac.local>
2019-03-26 17:26:23 +0200

    update

--- src/custom-bottomsheet.ios.ts
+++ src/custom-bottomsheet.ios.ts
@@ -1,13 +1,55 @@
+/// <reference path="./typings/cbs.android.d.ts" />
+import { CBSheetOption } from "./custom-bottomsheet.common";
+import * as app from 'tns-core-modules/application';
+import * as utils from 'tns-core-modules/utils/utils';
 
 
 export class CustomBottomsheet {
 
+    private _cbsDelegateImpl: LCActionSheetDelegateImpl;
+
     constructor() {
 
-    }
-
-    public show(): void {
+        this._cbsDelegateImpl = LCActionSheetDelegateImpl.new();
 
     }
 
+    public show(options: any): void {
+        const bottomSheet = LCActionSheet.sheetWithTitleDelegateCancelButtonTitleOtherButtonTitleArray("TEST", this._cbsDelegateImpl, 'Cancel', ['Test 1', 'Test 2', 'Test 3']);
+
+        bottomSheet.show();
+    }
 }
+
+class LCActionSheetDelegateImpl extends NSObject implements LCActionSheetDelegate {
+    static objCProtocols = [LCActionSheetDelegate];
+
+    static new(): LCActionSheetDelegateImpl {
+        return <LCActionSheetDelegateImpl>super.new();
+    }
+
+    public actionSheetClickedButtonAtIndex(actionSheet, index): void {
+        console.log(index);
+    }
+
+    public willPresentActionSheet(actionSheet): void {
+        console.log('will present')
+    }
+
+    public didPresentActionSheet(actionSheet): void {
+        console.log('did present');
+    }
+
+    public actionSheetWillDismissWithButtonIndex(actionSheet, index): void {
+        console.log('Button present');
+    }
+
+    public actionSheetDidDismissWithButtonIndex(actionSheet, index): void {
+        console.log('rrrrr')
+    }
+}
+
+const rootVC = function () {
+    let appwindow = UIApplication.sharedApplication.keyWindow;
+    return appwindow.rootViewController;
+};
---
+++ src/typings/cbs.ios.d.ts
@@ -0,0 +1,261 @@
+
+declare class LCActionSheet extends UIView {
+
+	static alloc(): LCActionSheet; // inherited from NSObject
+
+	static appearance(): LCActionSheet; // inherited from UIAppearance
+
+	static appearanceForTraitCollection(trait: UITraitCollection): LCActionSheet; // inherited from UIAppearance
+
+	static appearanceForTraitCollectionWhenContainedIn(trait: UITraitCollection, ContainerClass: typeof NSObject): LCActionSheet; // inherited from UIAppearance
+
+	static appearanceForTraitCollectionWhenContainedInInstancesOfClasses(trait: UITraitCollection, containerTypes: NSArray<typeof NSObject> | typeof NSObject[]): LCActionSheet; // inherited from UIAppearance
+
+	static appearanceWhenContainedIn(ContainerClass: typeof NSObject): LCActionSheet; // inherited from UIAppearance
+
+	static appearanceWhenContainedInInstancesOfClasses(containerTypes: NSArray<typeof NSObject> | typeof NSObject[]): LCActionSheet; // inherited from UIAppearance
+
+	static new(): LCActionSheet; // inherited from NSObject
+
+	static sheetWithTitleCancelButtonTitleClickedOtherButtonTitleArray(title: string, cancelButtonTitle: string, clickedHandler: (p1: LCActionSheet, p2: number) => void, otherButtonTitleArray: NSArray<string> | string[]): LCActionSheet;
+
+	static sheetWithTitleCancelButtonTitleClickedOtherButtonTitles(title: string, cancelButtonTitle: string, clickedHandler: (p1: LCActionSheet, p2: number) => void, otherButtonTitles: string): LCActionSheet;
+
+	static sheetWithTitleCancelButtonTitleDidDismissOtherButtonTitleArray(title: string, cancelButtonTitle: string, didDismissHandler: (p1: LCActionSheet, p2: number) => void, otherButtonTitleArray: NSArray<string> | string[]): LCActionSheet;
+
+	static sheetWithTitleCancelButtonTitleDidDismissOtherButtonTitles(title: string, cancelButtonTitle: string, didDismissHandler: (p1: LCActionSheet, p2: number) => void, otherButtonTitles: string): LCActionSheet;
+
+	static sheetWithTitleDelegateCancelButtonTitleOtherButtonTitleArray(title: string, delegate: LCActionSheetDelegate, cancelButtonTitle: string, otherButtonTitleArray: NSArray<string> | string[]): LCActionSheet;
+
+	static sheetWithTitleDelegateCancelButtonTitleOtherButtonTitles(title: string, delegate: LCActionSheetDelegate, cancelButtonTitle: string, otherButtonTitles: string): LCActionSheet;
+
+	animationDuration: number;
+
+	autoHideWhenDeviceRotated: boolean;
+
+	blurBackgroundColor: UIColor;
+
+	blurEffectStyle: UIBlurEffectStyle;
+
+	buttonBgColor: UIColor;
+
+	buttonColor: UIColor;
+
+	buttonCornerRadius: number;
+
+	buttonEdgeInsets: UIEdgeInsets;
+
+	buttonFont: UIFont;
+
+	buttonHeight: number;
+
+	cancelButtonBgColor: UIColor;
+
+	cancelButtonColor: UIColor;
+
+	readonly cancelButtonIndex: number;
+
+	cancelButtonTitle: string;
+
+	clickedHandler: (p1: LCActionSheet, p2: number) => void;
+
+	darkOpacity: number;
+
+	darkViewNoTaped: boolean;
+
+	delegate: LCActionSheetDelegate;
+
+	destructiveButtonBgColor: UIColor;
+
+	destructiveButtonColor: UIColor;
+
+	destructiveButtonIndexSet: NSIndexSet;
+
+	didDismissHandler: (p1: LCActionSheet, p2: number) => void;
+
+	didPresentHandler: (p1: LCActionSheet) => void;
+
+	numberOfTitleLines: number;
+
+	redButtonIndexSet: NSIndexSet;
+
+	scrolling: boolean;
+
+	separatorColor: UIColor;
+
+	title: string;
+
+	titleColor: UIColor;
+
+	titleEdgeInsets: UIEdgeInsets;
+
+	titleFont: UIFont;
+
+	unBlur: boolean;
+
+	visibleButtonCount: number;
+
+	willDismissHandler: (p1: LCActionSheet, p2: number) => void;
+
+	willPresentHandler: (p1: LCActionSheet) => void;
+
+	constructor(o: { title: string; cancelButtonTitle: string; clicked: (p1: LCActionSheet, p2: number) => void; otherButtonTitleArray: NSArray<string> | string[]; });
+
+	constructor(o: { title: string; cancelButtonTitle: string; clicked: (p1: LCActionSheet, p2: number) => void; otherButtonTitles: string; });
+
+	constructor(o: { title: string; cancelButtonTitle: string; didDismiss: (p1: LCActionSheet, p2: number) => void; otherButtonTitleArray: NSArray<string> | string[]; });
+
+	constructor(o: { title: string; cancelButtonTitle: string; didDismiss: (p1: LCActionSheet, p2: number) => void; otherButtonTitles: string; });
+
+	constructor(o: { title: string; delegate: LCActionSheetDelegate; cancelButtonTitle: string; otherButtonTitleArray: NSArray<string> | string[]; });
+
+	constructor(o: { title: string; delegate: LCActionSheetDelegate; cancelButtonTitle: string; otherButtonTitles: string; });
+
+	appendButtonWithTitleAtIndex(title: string, index: number): void;
+
+	appendButtonsWithTitles(titles: string): void;
+
+	appendButtonsWithTitlesAtIndexes(titles: NSArray<string> | string[], indexes: NSIndexSet): void;
+
+	buttonTitleAtIndex(index: number): string;
+
+	initWithTitleCancelButtonTitleClickedOtherButtonTitleArray(title: string, cancelButtonTitle: string, clickedHandler: (p1: LCActionSheet, p2: number) => void, otherButtonTitleArray: NSArray<string> | string[]): this;
+
+	initWithTitleCancelButtonTitleClickedOtherButtonTitles(title: string, cancelButtonTitle: string, clickedHandler: (p1: LCActionSheet, p2: number) => void, otherButtonTitles: string): this;
+
+	initWithTitleCancelButtonTitleDidDismissOtherButtonTitleArray(title: string, cancelButtonTitle: string, didDismissHandler: (p1: LCActionSheet, p2: number) => void, otherButtonTitleArray: NSArray<string> | string[]): this;
+
+	initWithTitleCancelButtonTitleDidDismissOtherButtonTitles(title: string, cancelButtonTitle: string, didDismissHandler: (p1: LCActionSheet, p2: number) => void, otherButtonTitles: string): this;
+
+	initWithTitleDelegateCancelButtonTitleOtherButtonTitleArray(title: string, delegate: LCActionSheetDelegate, cancelButtonTitle: string, otherButtonTitleArray: NSArray<string> | string[]): this;
+
+	initWithTitleDelegateCancelButtonTitleOtherButtonTitles(title: string, delegate: LCActionSheetDelegate, cancelButtonTitle: string, otherButtonTitles: string): this;
+
+	show(): void;
+}
+
+declare class LCActionSheetCell extends UITableViewCell {
+
+	static alloc(): LCActionSheetCell; // inherited from NSObject
+
+	static appearance(): LCActionSheetCell; // inherited from UIAppearance
+
+	static appearanceForTraitCollection(trait: UITraitCollection): LCActionSheetCell; // inherited from UIAppearance
+
+	static appearanceForTraitCollectionWhenContainedIn(trait: UITraitCollection, ContainerClass: typeof NSObject): LCActionSheetCell; // inherited from UIAppearance
+
+	static appearanceForTraitCollectionWhenContainedInInstancesOfClasses(trait: UITraitCollection, containerTypes: NSArray<typeof NSObject> | typeof NSObject[]): LCActionSheetCell; // inherited from UIAppearance
+
+	static appearanceWhenContainedIn(ContainerClass: typeof NSObject): LCActionSheetCell; // inherited from UIAppearance
+
+	static appearanceWhenContainedInInstancesOfClasses(containerTypes: NSArray<typeof NSObject> | typeof NSObject[]): LCActionSheetCell; // inherited from UIAppearance
+
+	static new(): LCActionSheetCell; // inherited from NSObject
+
+	buttonEdgeInsets: UIEdgeInsets;
+
+	cellSeparatorColor: UIColor;
+
+	lineView: UIView;
+
+	titleLabel: UILabel;
+}
+
+declare class LCActionSheetConfig extends NSObject {
+
+	static alloc(): LCActionSheetConfig; // inherited from NSObject
+
+	static new(): LCActionSheetConfig; // inherited from NSObject
+
+	static shared(): LCActionSheetConfig;
+
+	animationDuration: number;
+
+	autoHideWhenDeviceRotated: boolean;
+
+	blurBackgroundColor: UIColor;
+
+	blurEffectStyle: UIBlurEffectStyle;
+
+	buttonBgColor: UIColor;
+
+	buttonColor: UIColor;
+
+	buttonCornerRadius: number;
+
+	buttonEdgeInsets: UIEdgeInsets;
+
+	buttonFont: UIFont;
+
+	buttonHeight: number;
+
+	cancelButtonBgColor: UIColor;
+
+	cancelButtonColor: UIColor;
+
+	readonly cancelButtonIndex: number;
+
+	cancelButtonTitle: string;
+
+	darkOpacity: number;
+
+	darkViewNoTaped: boolean;
+
+	destructiveButtonBgColor: UIColor;
+
+	destructiveButtonColor: UIColor;
+
+	destructiveButtonIndexSet: NSIndexSet;
+
+	numberOfTitleLines: number;
+
+	scrolling: boolean;
+
+	separatorColor: UIColor;
+
+	title: string;
+
+	titleColor: UIColor;
+
+	titleEdgeInsets: UIEdgeInsets;
+
+	titleFont: UIFont;
+
+	unBlur: boolean;
+
+	visibleButtonCount: number;
+
+	static readonly config: LCActionSheetConfig;
+}
+
+interface LCActionSheetDelegate extends NSObjectProtocol {
+
+	actionSheetClickedButtonAtIndex?(actionSheet: LCActionSheet, buttonIndex: number): void;
+
+	actionSheetDidDismissWithButtonIndex?(actionSheet: LCActionSheet, buttonIndex: number): void;
+
+	actionSheetWillDismissWithButtonIndex?(actionSheet: LCActionSheet, buttonIndex: number): void;
+
+	didPresentActionSheet?(actionSheet: LCActionSheet): void;
+
+	willPresentActionSheet?(actionSheet: LCActionSheet): void;
+}
+declare var LCActionSheetDelegate: {
+
+	prototype: LCActionSheetDelegate;
+};
+
+declare var LCActionSheetVersionNumber: number;
+
+declare var LCActionSheetVersionString: interop.Reference<number>;
+
+declare class LCActionSheetViewController extends UIViewController {
+
+	static alloc(): LCActionSheetViewController; // inherited from NSObject
+
+	static new(): LCActionSheetViewController; // inherited from NSObject
+
+	statusBarHidden: boolean;
+
+	statusBarStyle: UIStatusBarStyle;
+}
