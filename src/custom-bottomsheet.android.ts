10ca1eb5807636894b80a4ebbe50ae8cdd05c367
Costa Caravotas <costacaravotas@Costas-iMac.local>
2019-04-01 18:09:23 +0200

    updat android class

--- src/custom-bottomsheet.android.ts
+++ src/custom-bottomsheet.android.ts
@@ -1,6 +1,6 @@
-/// <reference path="./typings/cbs.android.d.ts" />
+/// <reference path='./typings/cbs.android.d.ts' />
 
-import { CBSheetOption } from "./custom-bottomsheet.common";
+import { CBSheetOption } from './custom-bottomsheet.common';
 import * as app from 'tns-core-modules/application';
 import * as utils from 'tns-core-modules/utils/utils';
 
@@ -21,6 +21,7 @@ export class CustomBottomsheet {
         bottomSheetFragment.setOnItemClickListener(new com.eljo.skhye05.custombottomsheet.CustomBottomSheetFragment.OnItemClickListener({
             onClick: (position) => {
                 options.onItemTap(position, options.items[position]);
+                bottomSheetFragment.dismiss()
             }
         }));
 
